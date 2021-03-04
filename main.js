517fc4a2bd4bb16b51244b8decf066810869f094
Elvis Vidal da Silva <elvisvidal@Elviss-MacBook-Pro.local>
2021-03-04 15:29:39 +0000

    L2-end

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Boots',
+      description: `
+        Built with utilitarian influence to keep your new season footwear rotation going strong, the Bonny chukka boot arrives from Dr. Martens.
+        The black fabric upper is crafted with extra tough nylon for durability, smooth overlays and an AirWair sole unit.
+      `
+    }
+  }
+});
